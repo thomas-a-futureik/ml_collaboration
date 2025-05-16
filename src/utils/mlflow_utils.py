52fcfa77c0902c2223e1fb5a574a3b3629898388
thomas-a-futureik <thomas.a@futureik.com>
2025-05-16 14:27:45 +0530

    mlflow utils

---
+++ src/utils/mlflow_utils.py
@@ -0,0 +1,23 @@
+import mlflow
+import os
+import sys
+
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+
+def get_experiment_id(name):
+    exp = mlflow.get_experiment_by_name(name)
+    if exp is None:
+      exp_id = mlflow.create_experiment(name)
+      return exp_id
+    return exp.experiment_id
+
+def get_last_run_id(exp_name):
+    exp = get_experiment_id(exp_name)
+    client = mlflow.MlflowClient()
+    runs = client.search_runs(experiment_ids=exp)
+    if len(runs) == 0:
+        return None
+    last_run = runs[0]
+    return last_run.info.run_id
+
+
